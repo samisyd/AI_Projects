896bf55bb20a7b5d9ef2ee6f69db951a99aa2acd
Samina <111076416+samisyd@users.noreply.github.com>
2026-06-26 16:42:29 -0700

    Delete Google_Projects/travel_assistant directory

--- Google_Projects/travel_assistant/Travel_Task1/__init__.py
+++
@@ -1,4 +0,0 @@
-# Travel Team Package - Task 6: Complete Demo
-from .agent import root_agent
-
-__all__ = ["root_agent"]
--- Google_Projects/travel_assistant/Travel_Task1/sub_agents/__init__.py
+++
@@ -1,6 +0,0 @@
-# Sub-agents Package
-from .flight_specialist import flight_specialist
-from .hotel_expert import hotel_expert
-from .activity_curator import activity_curator
-
-__all__ = ["flight_specialist", "hotel_expert", "activity_curator"]
